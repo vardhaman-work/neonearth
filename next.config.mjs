2f80baed8b0f9dfcbc7f55d77b37cf9400224a7c
vardhaman-work <vardhaman.work@gmail.com>
2025-04-02 16:39:31 +0530

    revert changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output: "export", // ✅ Enables static export
+    output: "export", // ✅ Enables static export
     images: {
       unoptimized: true, // ✅ Fix for images in static export
     },
