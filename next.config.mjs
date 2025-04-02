8323bbbcf1d1281d861d6e436a9b0bac10279cd0
vardhaman-work <vardhaman.work@gmail.com>
2025-04-02 17:41:29 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", // ✅ Enables static export
-    images: {
-      unoptimized: true, // ✅ Fix for images in static export
-    },
-    basePath: "/neonearth", // ✅ Change to your GitHub repo name
-    assetPrefix: "/neonearth/", // ✅ Prefix for assets
+    output: "export"
   };
   
   export default nextConfig;
